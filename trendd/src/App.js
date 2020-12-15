122ca8905e71d12030712eb1d73f3bc44b57192e
itzomen <peng.akebuon2468@gmail.com>
2020-12-15 17:54:02 +0100

    configured .env file and added API key

--- trendd/src/App.js
+++ trendd/src/App.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey = process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+        })
+    },[])
     return (
         <div>
             <h1>Trendd</h1>
