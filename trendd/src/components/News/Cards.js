e0294ac7bbe4a532c8f1b3e8aa330832554b5a47
itzomen <peng.akebuon2468@gmail.com>
2020-12-19 10:42:53 +0100

    Stlyled the Cards.js with material UI

--- trendd/src/components/News/Cards.js
+++ trendd/src/components/News/Cards.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Grid, Grow, Typography } from '@material-ui/core';
+
 import NewsCard from '../NewsCard/NewsCard';
 
 const NewsCards = ({ articles }) => {
     return (
-        <div>
-            {articles.map((articles, i) => (
-                <NewsCard />
-            ))}
-        </div>
+        <Grow in>
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {articles.map((articles, i) => (
+                    <Grid Item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex'}}>
+                        <NewsCard articles={articles} i={i}/>
+                    </Grid>
+                ))}
+            </Grid>
+            
+        </Grow>
     );
 }
 
