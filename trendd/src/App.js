5f957d98723d729f6f8fd88dc2254122823084d9
itzomen <peng.akebuon2468@gmail.com>
2020-12-19 10:22:41 +0100

    created Cards.js and styles.js

--- trendd/src/App.js
+++ trendd/src/App.js
@@ -1,16 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/News/Cards';
 
 const alanKey = process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
+    const [Articles, setArticles] = useState([]);
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
             onCommand: ({ command, articles }) => {
                 if(command === 'Headlines'){
-                    console.log(articles);
+                    setArticles(articles);
                 }
             }
         })
@@ -18,6 +20,7 @@ const App = () => {
     return (
         <div>
             <h1>Trendd</h1>
+            <NewsCards articles={Articles} />
         </div>
     )
 }
---
+++ trendd/src/components/News/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NewsCards = ({ articles }) => {
+    return (
+        <div>
+            {articles.map((articles, i) => (
+                <NewsCards />
+            ))}
+        </div>
+    );
+}
+
+export default NewsCards;
