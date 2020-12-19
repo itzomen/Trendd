fcdcfccad95cfae047552c82ef9f0acf5d148715
itzomen <peng.akebuon2468@gmail.com>
2020-12-19 12:28:41 +0100

    created cards and added contents

--- trendd/src/components/News/Cards.js
+++ trendd/src/components/News/Cards.js
@@ -9,9 +9,9 @@ const NewsCards = ({ articles }) => {
     return (
         <Grow in>
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {articles.map((articles, i) => (
-                    <Grid Item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex'}}>
-                        <NewsCard articles={articles} i={i}/>
+                {articles.map((article, i) => (
+                    <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex'}}>
+                        <NewsCard article={article} i={i}/>
                     </Grid>
                 ))}
             </Grid>
--- trendd/src/components/NewsCard/NewsCard.js
+++ trendd/src/components/NewsCard/NewsCard.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
 
-const NewsCard = () => {
+
+const NewsCard = ({ article, i}) => {
+    //console.log(article);
     return (
-        <div>
-            News Card
-        </div>
+        <Card>
+            <CardActionArea>
+                <CardMedia image={article.urlToImage || 'https://github.com/itzomen/Trendd/blob/main/Trendd.png' } />
+                    <div>
+                        <Typography variant="body2" color="textSecondary" component="h2">{( new Date(article.publishedAt)).toDateString()}</Typography>
+                        <Typography variant="body2" color="textSecondary" component="h2">{article.source.name}</Typography>
+                    </div>
+                    <Typography gutterBottom variant="h5">{article.title}</Typography>
+                    <CardContent>
+                        <Typography variant="body2" color="textSecondary" component="p">{article.description}</Typography>
+                    </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
+            </CardActions>
+        </Card>
     );
 }
 
