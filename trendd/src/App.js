1dcf72d40e43591fa301fae6c28084436f24521f
itzomen <peng.akebuon2468@gmail.com>
2021-02-14 12:13:13 +0100

    edited news card and added images

--- trendd/src/App.js
+++ trendd/src/App.js
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/News/Cards';
 
+import Logo from './images/Trendd.png';
+
+import useStyles from './styles';
+
 const alanKey = process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
     const [Articles, setArticles] = useState([]);
+
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -19,7 +25,9 @@ const App = () => {
     },[])
     return (
         <div>
-            <h1>Trendd</h1>
+            <div className={classes.logoContainer}>
+                <img src={Logo} className={classes.Logo} alt="logo" />
+            </div>
             <NewsCards articles={Articles} />
         </div>
     )
--- trendd/src/components/NewsCard/NewsCard.js
+++ trendd/src/components/NewsCard/NewsCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
 
+import Paper from '../../images/paper.jpg';
+
 import useStyles from './styles.js';
 
 
@@ -9,19 +11,19 @@ const NewsCard = ({ article, i}) => {
     const classes = useStyles();
 
     return (
-        <Card>
-            <CardActionArea>
-                <CardMedia className={classes.media} image={article.urlToImage || 'https://github.com/itzomen/Trendd/blob/main/Trendd.png' } />
-                    <div>
+        <Card className={ classes.card }>
+            <CardActionArea href={article.url} target="_blank">
+                <CardMedia className={classes.media} image={article.urlToImage || {Paper} } />
+                    <div className={ classes.details }>
                         <Typography variant="body2" color="textSecondary" component="h2">{( new Date(article.publishedAt)).toDateString()}</Typography>
                         <Typography variant="body2" color="textSecondary" component="h2">{article.source.name}</Typography>
                     </div>
-                    <Typography gutterBottom variant="h5">{article.title}</Typography>
+                    <Typography className={ classes.title } gutterBottom variant="h5">{article.title}</Typography>
                     <CardContent>
                         <Typography variant="body2" color="textSecondary" component="p">{article.description}</Typography>
                     </CardContent>
             </CardActionArea>
-            <CardActions>
+            <CardActions className={ classes.cardActions }>
                 <Button size="small" color="primary">Learn More</Button>
                 <Typography variant="h5" color="textSecondary">{i + 1}</Typography>
             </CardActions>
